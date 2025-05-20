ee46c812ed830ab2d3ca85d5d965470e443ab11c
AlexanderBeltranFerreres <abeltran3@iesebre.com>
2025-05-21 01:49:35 +0200

    canviat axios a veure si funciona ara be afegint /api al baseurl

--- g2app/resources/js/axios.js
+++ g2app/resources/js/axios.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-// Configuració global de axios
-axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'http://localhost:8000';
+const baseURL = (import.meta.env.VITE_API_URL || 'https://localhost:8000') + '/api';
+
+axios.defaults.baseURL = baseURL;
+axios.defaults.withCredentials = true;  // si fas servir sessions/cookies
 
 export default axios;
