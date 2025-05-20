a510a8bab73903a820120e07f680739f8c58f7fc
AlexanderBeltranFerreres <abeltran3@iesebre.com>
2025-05-21 01:06:48 +0200

    canviar axios.js per fer el baseURL amb la variable de entorn https

--- g2app/resources/js/axios.js
+++ g2app/resources/js/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 // Configuració global de axios
-axios.defaults.baseURL = 'http://localhost:8000';
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'http://localhost:8000';
 
 export default axios;
