338481359d91da9724a7c18b803ac3044fd96ed9
AlexanderBeltranFerreres <abeltran3@iesebre.com>
2025-05-21 02:44:58 +0200

    arreglat cors

--- g2app/config/cors.php
+++ g2app/config/cors.php
@@ -1,28 +1,38 @@
 <?php
+
 return [
 
-'paths' => [
-'api/*',
-'login',
-'logout',
-'register',
-'sanctum/csrf-cookie',
-],
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Aquí pots configurar els paràmetres de CORS per a la teva aplicació.
+    |
+    */
 
-'allowed_methods' => ['*'],
+    'paths' => [
+        'api/*',
+        'login',
+        'logout',
+        'register',
+        'sanctum/csrf-cookie',
+    ],
 
-'allowed_origins' => [
-'https://mp13-g2-tabletap.onrender.com',
-],
+    'allowed_methods' => ['*'],
 
-'allowed_origins_patterns' => [],
+    'allowed_origins' => [
+        'https://mp13-g2-tabletap.onrender.com',
+    ],
 
-'allowed_headers' => ['*'],
+    'allowed_origins_patterns' => [],
 
-'exposed_headers' => [],
+    'allowed_headers' => ['*'],
 
-'max_age' => 0,
+    'exposed_headers' => [],
 
-'supports_credentials' => true,
+    'max_age' => 0,
+
+    'supports_credentials' => true,
 
 ];
