1a7027a653842ff196bb813de73dee295b1b80ab
AlexanderBeltranFerreres <abeltran3@iesebre.com>
2025-05-26 17:04:20 +0200

    afegit enllaç simbolic al start.sh

--- g2app/start.sh
+++ g2app/start.sh
@@ -2,8 +2,14 @@
 
 echo "Starting Laravel on port ${PORT:-10000}"
 
+# MIGRACIONS
 php artisan migrate --force
 
+# SEEDERS (refresca i re-seedeja la base de dades)
 php artisan migrate:refresh --seed --seeder=Database\\Seeders\\MunicipiProvinciaSeeder --force
 
+# CREA L'ENLLAÇ SIMBÒLIC PER /storage
+php artisan storage:link
+
+# SERVEIX L'APP
 php artisan serve --host=0.0.0.0 --port=${PORT:-10000}
